c305e14d9d61abfd40dbe2e862c4500b6263c577
swayam-developer <swayamsathawane@gmail.com>
2025-05-29 10:33:59 +0530

    backend updates

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:8000/auth",
+  baseURL: "https://oauth2-backend.onrender.com/auth",
 });
 export const googleAuth = (code) => api.get(`/google?code=${code}`);
